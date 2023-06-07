6746f205d8df068b7bbe54eb2c55e9f87bfaa681
Develop and System <developandsystem@gmail.com>
2023-06-07 11:14:19 +0200

    update

--- inicio/03_dinamismo/utils/js/01_nodos.js
+++ inicio/03_dinamismo/utils/js/01_nodos.js
@@ -23,21 +23,49 @@ console.log(nodoBotones);*/
 
 let inputNodo = document.querySelector("input");
 let botonAnadir = document.querySelector("#boton-anadir");
-let lista = document.querySelector("div.lista-dos ul");
+let botonBorrar = document.querySelector("#boton-borrar");
+let lista1 = document.querySelector("div.lista-uno ul");
+let lista2 = document.querySelector("div.lista-dos ul");
+let select = document.querySelector("select");
+let li = document.querySelectorAll("li");
+
+li.forEach((e) => {
+  e.addEventListener("click", () => {
+    // se quitan todos los active
+    li.forEach((e1) => {
+      e1.classList.remove("active");
+    });
+
+    // se pone active el del click
+    e.classList.add("active");
+  });
+});
 
 botonAnadir.addEventListener("click", (e) => {
-  /* // 1 creo un lista 2
+  // 1 creo un lista 2
   let nodoLI = document.createElement("li");
   nodoLI.className = "list-group-item li-hijo";
   nodoLI.textContent = inputNodo.value;
   inputNodo.value = "";
   // 2 lo añado a la lista
-  lista.append(nodoLI); */
 
-  lista.innerHTML += `<li class='list-group-item li-hijo'>${inputNodo.value}</li>`;
+  //console.log(select.value);
+  if (Number(select.value) == 1) {
+    lista1.innerHTML += `<li class='list-group-item li-hijo'>${inputNodo.value}</li>`;
+  } else if (Number(select.value) == 2) {
+    lista2.innerHTML += `<li class='list-group-item li-hijo'>${inputNodo.value}</li>`;
+  } else {
+    alert("Por favor selecciona una lista");
+  }
+  // lista.innerHTML += `<li class='list-group-item li-hijo'>${inputNodo.value}</li>`;
+});
+
+botonBorrar.addEventListener("click", (e) => {
+  let nodoBorrar = document.querySelector("#nodo-borrar");
+  nodoBorrar.remove();
+  lista1.removeChild(nodoBorrar);
 });
 
 //console.log(botonAnadir);
-
-let nodos = document.querySelectorAll("div.lista-dos ul li");
-console.log(nodos);
+//let nodos = document.querySelectorAll("div.lista-dos ul li");
+//console.log(nodos);
