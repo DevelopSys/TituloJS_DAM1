23c832925e9182b19a210c874695ecbb294ab3c8
Develop and System <developandsystem@gmail.com>
2023-06-09 11:26:14 +0200

    Update curso.js

--- inicio/05_objetos/utils/js/ejercicios/curso.js
+++ inicio/05_objetos/utils/js/ejercicios/curso.js
@@ -1,7 +1,21 @@
 let dam1 = [
-  new Asignatura("Programacion", "PRO", 7, ["C1", "C2"], 1, "Borja Martin"),
+  new Asignatura(
+    "Programacion",
+    "PRO",
+    7,
+    ["C1", "C2", "C3", "C4"],
+    1,
+    "Borja Martin"
+  ),
   new Asignatura("Entornos", "ED", 3, ["C1", "C2"], 1, "Jesus Niño"),
-  new Asignatura("Base de datos", "BD", 6, ["C1", "C2"], 1, "Luis Hermida"),
+  new Asignatura(
+    "Base de datos",
+    "BD",
+    6,
+    ["C1", "C2", "C4"],
+    1,
+    "Luis Hermida"
+  ),
   new Asignatura(
     "Formacion Laboral",
     "FOL",
@@ -10,7 +24,7 @@ let dam1 = [
     1,
     "Ivan Redruello"
   ),
-  new Asignatura("Sistemas", "SI", 6, ["C1", "C2"], 1, "Sandra Leon"),
+  new Asignatura("Sistemas", "SI", 6, ["C1", "C2", "C4"], 1, "Sandra Leon"),
   new Asignatura("Lenguaje de marcas", "LM", 5, ["C1", "C2"], 1, "Sandra Leon"),
 ];
 let dam2 = [
@@ -18,15 +32,26 @@ let dam2 = [
     nombre: "Programacion Multimedia",
     siglas: "PMDM",
     horas: 4,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4", "C4"],
     curso: 2,
     profesor: "Borja Martin",
+    mostrarDatos: function () {
+      console.log(`Nombre: ${this.nombre}`);
+      console.log(`Siglas: ${this.nombre}`);
+      console.log(`Horas: ${this.nombre}`);
+      console.log(`Profesor: ${this.nombre}`);
+      console.log(`Curso: ${this.nombre}`);
+      console.log(`Los conocimientos son`);
+      this.conocimientos.forEach((e) => {
+        console.log(`\t ${e}`);
+      });
+    },
   },
   {
     nombre: "Desarrollo de Interfaces",
     siglas: "DI",
     horas: 6,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4", "C4", "C4"],
     curso: 2,
     profesor: "Borja Martin",
   },
@@ -34,7 +59,7 @@ let dam2 = [
     nombre: "Programacion Servicios y Procesos",
     siglas: "PSP",
     horas: 5,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4", "C4", "C4", "C4"],
     curso: 2,
     profesor: "Jesus Niño",
   },
@@ -50,7 +75,7 @@ let dam2 = [
     nombre: "Sistemas de Gestión Empresarial",
     siglas: "SGE",
     horas: 5,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4"],
     curso: 2,
     profesor: "Numilen Joost",
   },
@@ -58,7 +83,7 @@ let dam2 = [
     nombre: "Ingles",
     siglas: "ING",
     horas: 2,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4", "C4"],
     curso: 2,
     profesor: "Rocio Pizarro",
   },
@@ -66,7 +91,7 @@ let dam2 = [
     nombre: "Empresa e Iniciativa Emprendedora",
     siglas: "EIE",
     horas: 3,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4", "C4", "C4", "C4", "C4", "C4"],
     curso: 2,
     profesor: "Marta Jimenez",
   },
@@ -74,7 +99,7 @@ let dam2 = [
     nombre: "Formacion en centros de trabajo",
     siglas: "FCTs",
     horas: 370,
-    conocimientos: ["C1", "C2"],
+    conocimientos: ["C1", "C2", "C4", "C4", "C4", "C4", "C4", "C4"],
     curso: 2,
     profesor: "",
   },
@@ -82,17 +107,33 @@ let dam2 = [
     nombre: "Modulo proyecto",
     siglas: "PRO",
     horas: 30,
-    conocimientos: ["C1", "C2"],
+    conocimientos: [
+      "C1",
+      "C2",
+      "C4",
+      "C4",
+      "C4",
+      "C4",
+      "C4",
+      "C4",
+      "C4",
+      "C4",
+      "C4",
+    ],
     curso: 2,
     profesor: "",
   },
 ];
 
-let asignatura = {
-  nombre: "Programacion Multimedia",
-  siglas: "PMDM",
-  horas: 4,
-  conocimientos: ["C1", "C2"],
-  curso: 2,
-  profesor: "Borja Martin",
-};
+let dam = [dam2, dam1];
+
+// mostrar todas las asigntaruas de 1 que tengan
+// al menos 4 conocimientos
+
+dam.forEach((element) => {
+  element.forEach((element1) => {
+    if (element1.curso == 2 && element1.conocimientos.length >= 4) {
+      console.log(element1.mostrarDatos());
+    }
+  });
+});
