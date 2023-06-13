8006c96fba9e265b89ff7a2e6a6eba7936d36dd0
Develop and System <developandsystem@gmail.com>
2023-06-13 10:48:56 +0200

    update

--- inicio/06_promesas/utils/js/02_api.js
+++ inicio/06_promesas/utils/js/02_api.js
@@ -1,6 +1,29 @@
 let lista = document.querySelector("#lista-usuarios");
+let boton = document.querySelector("#boton-busqueda");
+let input = document.querySelector("#input-numero");
+let select = document.querySelector("#select-genero");
 
-fetch("https://randomuser.me/api/?results=50")
+boton.addEventListener("click", (e) => {
+  // sacar el numero del input
+  let numero = input.value;
+  let genero = select.value;
+  console.log(`${numero} ${genero}`);
+  // sacar el genero del select
+  fetch(`https://randomuser.me/api/?gender=${genero}&results=${numero}`)
+    .then((e) => e.json())
+    .then((e1) => {
+      e1.results.forEach((element) => {
+        lista.innerHTML += `<li>${element.name.title} ${element.name.first} ${element.name.last}</li>`;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // https://randomuser.me/api/?gender=all&results=100
+});
+
+/* fetch("https://randomuser.me/api/?results=50")
   .then((e) => e.json())
   .then((e1) => {
     e1.results.forEach((element) => {
@@ -9,7 +32,7 @@ fetch("https://randomuser.me/api/?results=50")
   })
   .catch((err) => {
     console.log(err);
-  });
+  }); */
 
 // incoporar un input para indicar el número de elementos a mostrar
 // incoporar un select para indicar el genero a mostrar (masculino,
