e38fe0af993d4cbfdb7f0f8df801ff4757bc0074
Develop and System <developandsystem@gmail.com>
2023-06-13 10:26:39 +0200

    update

--- inicio/06_promesas/utils/js/02_api.js
+++ inicio/06_promesas/utils/js/02_api.js
@@ -1,15 +1,18 @@
+let lista = document.querySelector("#lista-usuarios");
+
 fetch("https://randomuser.me/api/?results=50")
   .then((e) => e.json())
   .then((e1) => {
     e1.results.forEach((element) => {
-      console.log(
-        `${element.name.title} ${element.name.first} ${element.name.last}`
-      );
+      lista.innerHTML += `<li>${element.name.title} ${element.name.first} ${element.name.last}</li>`;
     });
   })
   .catch((err) => {
     console.log(err);
   });
+
+// incoporar un input para indicar el número de elementos a mostrar
+// incoporar un select para indicar el genero a mostrar (masculino, femenino, todas)
 
 // Crear una ul y meter todos los usuarios (nombre - apellidos)
 // dentro de la misma
